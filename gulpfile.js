ab3841104b5c7a50576e20f6bac298cfc4d7d9ea
Lisa Anderson <lisachananderson@gmail.com>
2022-03-01 17:59:22 -0800

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+/* Install and configure Gulp for your assignment. Create a Gulp file and have Gulp handle the compilation of your SassScript. */
+
+var {src, dest, watch} = require('gulp');
+var sass = require('gulp-sass')(require('node-sass'));
+sass.compiler = require('node-sass');
+
+function css() {
+    return src('src/*.scss')
+        .pipe(sass())
+        .pipe(dest('dist/css'))
+}
+exports.default = function () {
+    watch('src/*.scss', css);
+    watch('src/modules/*.scss', css);
+};
